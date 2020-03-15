e1d33b05e25fb80fa6c7983d4d3c9940a8d06750
BENAHMED DAHO Ali <bidandou@yahoo.fr>
2020-03-15 01:09:09 +0100

    Working Home Page

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 400,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
 const ServiceCard = (props) => {
+    const classes = useStyles();
     return (
-        <div>
-            <Card>
-                <CardMedia style={{ height: 0, paddingTop: '56.25%' }}
+        <Grid
+            item
+            xs={12}
+            md={4}
+            style={{ textAlign: "center", padding: "2rem" }}
+        >
+            <Card className={classes.root}>
+                <CardMedia 
+                    className={classes.media}
                     image={props.service.image}
                     title={props.service.title}
                 />
@@ -28,9 +46,8 @@ const ServiceCard = (props) => {
                         </Button>
                 </CardActions>
             </Card>
-        </div>
+        </Grid>
     );
-
 }
 
 export default ServiceCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useTranslate } from "react-translate";
 
 import NavBar from '../components/NavBar';
-import ServicesList from '../components/ServicesList';
+import ServiceCard from '../components/ServiceCard';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +20,11 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Home = ({ languageMode, handleLanguageToggle }) => {
+
+  const proServices = [{ title: 'Blockchain', description: 'Blockchain Based Solutions', image: "/static/images/blockchain-logo.jpg" },
+                       { title: 'GIS', description: 'Geographical Informations Systems (GIS)', image: "/static/images/gis.png" },
+                       { title: 'SDS', description: 'Spatial Data Science', image: "/static/images/gis.png" }];
+
   let t = useTranslate(languageMode ? "english" : "arabic");
   const classes = useStyles();
 
@@ -31,8 +36,8 @@ const Home = ({ languageMode, handleLanguageToggle }) => {
           handleLanguageToggle={handleLanguageToggle}
         />
         <div className={classes.root}>
-          <Grid container spacing={3} alignItems="center">
-            <Grid item xs={12} md={6} alignItems="center">
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
               <div className="opening-blurb">
                 <Typography variant="h2">{t("What")} </Typography>
                 <hr />
@@ -41,36 +46,39 @@ const Home = ({ languageMode, handleLanguageToggle }) => {
             </Grid>
           </Grid>
         </div>
-
-        <div className="blurb">
-          <Grid container spacing={3}>
-            <Grid
-              item
-              xs={12}
-              md={12}
-              style={{ textAlign: "center", padding: "5rem" }}
-            >
-              <Typography variant="h3" style={{ color: "white" }}> {t("citation80pp")} </Typography>
-            </Grid>
+      </div>
+      <div className="blurb">
+        <Grid container spacing={3}>
+          <Grid
+            item
+            xs={12}
+            md={12}
+            style={{ textAlign: "center", padding: "5rem" }}
+          >
+            <Typography variant="h3" style={{ color: "white" }}> {t("citation80pp")} </Typography>
           </Grid>
-        </div>
+        </Grid>
+      </div>
 
-        <div className="howitworks">
-          <Grid container spacing={3}>
-            <Grid
-              item
-              xs={12}
-              md={12}
-              style={{ textAlign: "center", padding: "2rem" }}
-            >
-              <Typography variant="h2">{t("services")} </Typography>
-            </Grid>
-
-            <ServicesList />
+      <div className="howitworks">
+        <Grid container spacing={3}>
+          <Grid
+            item
+            xs={12}
+            md={12}
+            style={{ textAlign: "center", padding: "2rem" }}
+          >
+            <Typography variant="h2">{t("services")} </Typography>
           </Grid>
+
+          {proServices.map(service => (                    
+                    <ServiceCard service={service}  key={service.title}/>
+            ))}
+
+        </Grid>
         |</div>
 
-      </div>
+
     </Fragment>
   );
 }
