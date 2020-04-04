856d03c2a000742c9b6c2119989e58a35fbeb001
BENAHMED DAHO Ali <bidandou@yahoo.fr>
2020-04-04 22:07:07 +0100

    Updates

---
+++ precache-manifest.acfbed93cffb087346967029c7e10a5e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d13a4f953653e8d2f4dd96bd4c43022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49bf82ffa94b8565e7d",
+    "url": "/static/css/main.bd812c15.chunk.css"
+  },
+  {
+    "revision": "7bd31284feb63f06a3c7",
+    "url": "/static/js/2.2f1cfedc.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.2f1cfedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49bf82ffa94b8565e7d",
+    "url": "/static/js/main.73fe5184.chunk.js"
+  },
+  {
+    "revision": "47e721882ab071addd24",
+    "url": "/static/js/runtime-main.2b6a863e.js"
+  },
+  {
+    "revision": "74d875c13e0cc25c4b734a2b648ad66a",
+    "url": "/static/media/enlightme-process.74d875c1.png"
+  },
+  {
+    "revision": "e1c1d572f6a53c9901567c874aaf1496",
+    "url": "/static/media/geo-use-cases.e1c1d572.jpg"
+  },
+  {
+    "revision": "b775c2b898b259d41bce4deff39f56ea",
+    "url": "/static/media/geodatasets.b775c2b8.jpg"
+  },
+  {
+    "revision": "eb2718dac43c6e39b9366320bb3692e3",
+    "url": "/static/media/li-platforms.eb2718da.png"
+  },
+  {
+    "revision": "bc9272d6ca403736ad11f355e5357b77",
+    "url": "/static/media/logo-enlightme-1.bc9272d6.png"
+  },
+  {
+    "revision": "918a6d5664568cc4078fe971d69c03c2",
+    "url": "/static/media/vision-process.918a6d56.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enlightme-website/precache-manifest.cf9c52bdc782b478e789279c1b7ad76f.js"
+  "/precache-manifest.acfbed93cffb087346967029c7e10a5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/enlightme-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
